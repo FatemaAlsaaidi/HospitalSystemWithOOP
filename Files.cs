f8ad47db9db77129607ae7f4083fb4c2d09d397a
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-07-23 09:53:11 +0400

    save data of doctore in doctor file

--- Files.cs
+++ Files.cs
@@ -14,5 +14,17 @@ namespace HospitalSystemWithOOP
         public static string PatientFile = "patients.txt";
         // file to save appointment data
         public static string AppointmentFile = "appointments.txt";
+
+        // save data of doctore in doctor file 
+        public static void SaveDoctorToFile(List<Doctor> doctors)
+        {
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(DoctorFile))
+            {
+                foreach (Doctor doctor in doctors)
+                {
+                    file.WriteLine($"{doctor.Id}, {doctor.Name}, {doctor.Specialization}");
+                }
+            }
+        }
     }
 }

44a86dfae6ca5b67bd11e240e9e4a73dca245743
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-07-23 09:47:16 +0400

    declare files for doctor , patient and appointment data

---
+++ Files.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalSystemWithOOP
+{
+    class Files
+    {
+        // file to save doctore data 
+        public static string DoctorFile = "doctors.txt";
+        // file to save patient data
+        public static string PatientFile = "patients.txt";
+        // file to save appointment data
+        public static string AppointmentFile = "appointments.txt";
+    }
+}
